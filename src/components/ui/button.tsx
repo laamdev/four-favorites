58519edc71a3faaf7645fb470d2639f969aa6f54
Luis Anaya <brothersailorwebdev@gmail.com>
2025-03-01 02:00:28 +0100

    fix: secondary button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -15,7 +15,7 @@ const buttonVariants = cva(
         destructive:
           'bg-destructive text-destructive-foreground shadow-xs hover:bg-destructive/90',
         outline:
-          'bg-transparent border-primary text-primary border-2 hover:border-zinc-950 hover:bg-zinc-950 hover:text-zinc-50',
+          'bg-transparent border-primary text-primary border-2 hover:border-transparent hover:bg-zinc-50 hover:text-zinc-950',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
         criterion:
